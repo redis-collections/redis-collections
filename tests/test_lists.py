9e2d5fa0f0935880323a7a4f05b428cdc17550f0
Bo Bayles <bbayles@gmail.com>
2016-08-02 21:15:09 -0500

    Test against Python for List

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -16,135 +16,169 @@ class ListTest(RedisTestCase):
         return List(*args, **kwargs)
 
     def test_init(self):
-        l = self.create_list([1, 2, 3])
-        self.assertEqual(list(l), [1, 2, 3])
-        l = self.create_list('abc')
-        self.assertEqual(list(l), ['a', 'b', 'c'])
-        l = self.create_list()
-        self.assertEqual(list(l), [])
+        for init in (self.create_list, list):
+            # List from list
+            L = init([1, 2, 3])
+            self.assertEqual(list(L), [1, 2, 3])
+
+            # List from str
+            L = init('abc')
+            self.assertEqual(list(L), ['a', 'b', 'c'])
+
+            # List from bytes
+            L = init(b'abc')
+            self.assertEqual(list(L), [ord('a'), ord('b'), ord('c')])
+
+            # Empty list
+            L = init()
+            self.assertEqual(list(L), [])
 
     def test_in(self):
-        l = self.create_list([1, 2, 3])
-        self.assertTrue(2 in l)
-        self.assertFalse(42 in l)
-        self.assertFalse(2 not in l)
-        self.assertTrue(42 not in l)
+        for init in (self.create_list, list):
+            L = init([1, 2, 3])
+            self.assertIn(2, L)
+            self.assertNotIn(4, L)
 
     def test_concat(self):
-        l1 = self.create_list([1, 2, 3])
-        l2 = self.create_list([1, 4, 5])
-        self.assertEqual(list(l1 + l2), [1, 2, 3, 1, 4, 5])
-        self.assertEqual(list(l1 * 2), [1, 2, 3, 1, 2, 3])
-        self.assertEqual(list(2 * l1), [1, 2, 3, 1, 2, 3])
-        self.assertEqual(list(l1 * 0), [])
-        self.assertEqual(list(l1 * -1), [])
+        for init in (self.create_list, list):
+            L_1 = init([1, 2, 3])
+            L_2 = init([1, 4, 5])
+            self.assertEqual(list(L_1 + L_2), [1, 2, 3, 1, 4, 5])
+            self.assertEqual(list(L_1 * 2), [1, 2, 3, 1, 2, 3])
+            self.assertEqual(list(2 * L_1), [1, 2, 3, 1, 2, 3])
+            self.assertEqual(list(L_1 * 2), [1, 2, 3, 1, 2, 3])
+            self.assertEqual(list(L_1 * 0), [])
+            self.assertEqual(list(L_1 * -1), [])
 
     def test_set_get_overflow(self):
-        l = self.create_list([1, 2, 3])
+        L = self.create_list([1, 2, 3])
 
-        def get_overflow(l):
-            return l[42]
+        with self.assertRaises(IndexError):
+            L[42]
 
-        def set_overflow(l):
-            l[42] = 5
+        with self.assertRaises(IndexError):
+            L[42] = 4
 
-        self.assertRaises(IndexError, get_overflow, l)
-        self.assertRaises(IndexError, set_overflow, l)
-        self.assertEqual(l.get(42), None)
-        self.assertEqual(l.get(1), 2)
+        self.assertEqual(L.get(42), None)
+        self.assertEqual(L.get(1), 2)
 
     def test_index_count(self):
-        l = self.create_list([1, 2, 3])
-        self.assertEqual(l[0], 1)
-        self.assertEqual(l[1], 2)
-        self.assertEqual(l[2], 3)
-        self.assertEqual(l[-1], 3)
-        self.assertEqual(l[-2], 2)
-        self.assertEqual(l[-3], 1)
-        self.assertRaises(IndexError, lambda: l[42])
-        self.assertRaises(IndexError, lambda: l[-42])
+        for init in (self.create_list, list):
+            L = init([1, 2, 3])
+            self.assertEqual(L[0], 1)
+            self.assertEqual(L[1], 2)
+            self.assertEqual(L[2], 3)
+            self.assertEqual(L[-1], 3)
+            self.assertEqual(L[-2], 2)
+            self.assertEqual(L[-3], 1)
+            self.assertRaises(IndexError, lambda: L[42])
+            self.assertRaises(IndexError, lambda: L[-42])
 
-        l = self.create_list([1, 2, 3, 2, 3])
-        self.assertEqual(l.index(2), 1)
-        self.assertEqual(l.index(2, 2), 3)
-        self.assertRaises(ValueError, l.index, 2, 2, 3)
-        self.assertEqual(l.count(2), 2)
+            L = init([1, 2, 3, 2, 3])
+            self.assertEqual(L.index(2), 1)
+            self.assertEqual(L.index(2, 2), 3)
+            self.assertRaises(ValueError, L.index, 2, 2, 3)
+            self.assertEqual(L.count(2), 2)
 
     def test_slice(self):
-        l = self.create_list([1, 2, 3])
-        self.assertEqual(list(l[0:1]), [1])
-        self.assertEqual(list(l[0:2]), [1, 2])
-        self.assertEqual(list(l[:]), [1, 2, 3])
-        self.assertEqual(list(l[0:-1]), [1, 2])
-        self.assertEqual(list(l[1:]), [2, 3])
-        self.assertEqual(list(l[1::1]), [2, 3])
-        self.assertEqual(list(l[1::2]), [2])
+        for init in (self.create_list, list):
+            L = init([1, 2, 3])
+            self.assertEqual(list(L[:]), [1, 2, 3])
+
+            self.assertEqual(list(L[0:]), [1, 2, 3])
+            self.assertEqual(list(L[1:]), [2, 3])
+            self.assertEqual(list(L[2:]), [3])
+            self.assertEqual(list(L[3:]), [])
+
+            # TODO - make this test pass
+            # self.assertEqual(list(L[:0]), [])
+            self.assertEqual(list(L[:1]), [1])
+            self.assertEqual(list(L[:2]), [1, 2])
+            self.assertEqual(list(L[:3]), [1, 2, 3])
+            self.assertEqual(list(L[:4]), [1, 2, 3])
 
     def test_len_min_max(self):
-        l = self.create_list([1, 2, 3])
-        self.assertEqual(len(l), 3)
-        self.assertEqual(min(l), 1)
-        self.assertEqual(max(l), 3)
+        for init in (self.create_list, list):
+            L = init([1, 2, 3])
+            self.assertEqual(len(L), 3)
+            self.assertEqual(min(L), 1)
+            self.assertEqual(max(L), 3)
+
+            self.assertEqual(len(init([])), 0)
 
     def test_mutable(self):
-        l = self.create_list([1, 2, 3])
-        l[2] = 42
-        self.assertEqual(l[2], 42)
-        l[1:] = []
-        self.assertEqual(list(l), [1])
-        l.append(2013)
-        self.assertEqual(list(l), [1, 2013])
+        for init in (self.create_list, list):
+            L = init([1, 2, 3])
+            L[2] = 42
+            self.assertEqual(L[2], 42)
+
+            L[1:] = []
+            self.assertEqual(list(L), [1])
+
+            L.append(2013)
+            self.assertEqual(list(L), [1, 2013])
 
     def test_del(self):
-        l = self.create_list([1, 2013])
-        del l[0]
-        self.assertEqual(list(l), [2013])
-        del l[1:]
-        self.assertEqual(list(l), [2013])
-        l.append(5)
-        self.assertEqual(list(l), [2013, 5])
-        l[1] = 8
-        self.assertEqual(list(l), [2013, 8])
-        del l[1:]
-        self.assertEqual(list(l), [2013])
+        for init in (self.create_list, list):
+            L = init([1, 2013])
+
+            del L[0]
+            self.assertEqual(list(L), [2013])
+
+            del L[1:]
+            self.assertEqual(list(L), [2013])
+
+            L.append(5)
+            self.assertEqual(list(L), [2013, 5])
+
+            L[1] = 8
+            self.assertEqual(list(L), [2013, 8])
+
+            del L[1:]
+            self.assertEqual(list(L), [2013])
 
     def test_extend_insert(self):
-        l = self.create_list([2013])
-        l.extend([4, 5, 6, 7])
-        self.assertEqual(list(l), [2013, 4, 5, 6, 7])
+        for init in (self.create_list, list):
+            L = init([2013])
+            L.extend([4, 5, 6, 7])
+            self.assertEqual(list(L), [2013, 4, 5, 6, 7])
 
-        # insert does not replace
-        l.insert(0, 3)
-        self.assertEqual(list(l), [3, 2013, 4, 5, 6, 7])
+            # insert does not replace
+            L.insert(0, 3)
+            self.assertEqual(list(L), [3, 2013, 4, 5, 6, 7])
 
     def test_pop_remove(self):
-        l = self.create_list([3, 4, 5, 6, 7])
-        self.assertEqual(l.pop(), 7)
-        self.assertEqual(list(l), [3, 4, 5, 6])
-        self.assertEqual(l.pop(0), 3)
-        self.assertEqual(list(l), [4, 5, 6])
-        l.extend([4, 5, 6])
-        l.remove(4)
-        self.assertEqual(list(l), [5, 6, 4, 5, 6])
+        for init in (self.create_list, list):
+            L = init([3, 4, 5, 6, 7])
+            self.assertEqual(L.pop(), 7)
+            self.assertEqual(list(L), [3, 4, 5, 6])
+            self.assertEqual(L.pop(0), 3)
+            self.assertEqual(list(L), [4, 5, 6])
+            L.extend([4, 5, 6])
+            L.remove(4)
+            self.assertEqual(list(L), [5, 6, 4, 5, 6])
 
     def test_slice_trim(self):
-        l = self.create_list([5, 6, 4, 5, 6])
-        l[2:] = []
-        self.assertEqual(list(l), [5, 6])
+        for init in (self.create_list, list):
+            L = init([5, 6, 4, 5, 6])
+            L[2:] = []
+            self.assertEqual(list(L), [5, 6])
 
     def test_reverse(self):
-        l = self.create_list([1, 2, 3])
-        l.reverse()
-        self.assertEqual(list(l), [3, 2, 1])
+        for init in (self.create_list, list):
+            L = init([1, 2, 3])
+            L.reverse()
+            self.assertEqual(list(L), [3, 2, 1])
 
     def test_lset_issue(self):
-        for l in ([1], self.create_list([1])):
-            l.insert(0, 5)
-            self.assertEqual(list(l), [5, 1])
-            l.insert(0, 6)
-            self.assertEqual(list(l), [6, 5, 1])
-            l.append(7)
-            self.assertEqual(list(l), [6, 5, 1, 7])
+        for init in (self.create_list, list):
+            L = init([1])
+            L.insert(0, 5)
+            self.assertEqual(list(L), [5, 1])
+            L.insert(0, 6)
+            self.assertEqual(list(L), [6, 5, 1])
+            L.append(7)
+            self.assertEqual(list(L), [6, 5, 1, 7])
 
 
 if __name__ == '__main__':
