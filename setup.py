daf7361d2e26e4e11bc36e5173f89efc0e120bf2
Bo Bayles <bbayles@gmail.com>
2016-08-08 21:03:41 -0500

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Database',
     )
 )
