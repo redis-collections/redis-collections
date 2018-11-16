98217f7fc7edda7897736c1d49cf315fd7b7d154
Bo Bayles <bbayles@gmail.com>
2018-11-16 12:12:03 -0600

    Placate new flake8

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -57,13 +57,15 @@ class DictTest(RedisTestCase):
         python_dict = dict(init_seq)
         six.assertCountEqual(self, redis_dict.items(), python_dict.items())
 
-        init_dict = {
-            'a': 1,
-            b'a': 2,
-            u'a': 3,
-            1: 'one',
-            1.0: 'one point zero',
-        }
+        init_dict = dict(
+            [
+                ('a', 1),
+                (b'a', 2),
+                (u'a', 3),
+                (1, 'one'),
+                (1.0, 'one point zero'),
+            ]
+        )
         redis_dict = self.create_dict(init_dict)
         python_dict = dict(init_dict)
         six.assertCountEqual(self, redis_dict.items(), python_dict.items())
