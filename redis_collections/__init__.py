755188397e1d3ec3455bc5edce09e6003cdad84b
Bo Bayles <bbayles@gmail.com>
2019-02-02 08:05:30 -0600

    Bump version to 0.6.0

--- redis_collections/__init__.py
+++ redis_collections/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import division, print_function, unicode_literals
 
 __title__ = 'redis-collections'
-__version__ = '0.5.2'
+__version__ = '0.6.0'
 __author__ = 'Honza Javorek'
 __license__ = 'ISC'
 __copyright__ = 'Copyright 2013-? Honza Javorek'
